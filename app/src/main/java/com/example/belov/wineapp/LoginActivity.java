ea0167b6ceb7c82218a12a81e34a2f53949ff5dc
alexgbelov <albelov@umd.edu>
2015-11-18 22:10:13 -0500

    Added a LoginActivity class.

--- app/src/main/java/com/example/belov/wineapp/LoginActivity.java
+++ app/src/main/java/com/example/belov/wineapp/LoginActivity.java
@@ -4,16 +4,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private String mUserName;
+    private String mPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
+        final EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
+        final EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
+        final Button loginButton = (Button) findViewById(R.id.loginButton);
+        final Button registerButton = (Button) findViewById(R.id.registerButton);
 
-    @Override
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mUserName = usernameEditText.getText().toString();
+                mPassword = passwordEditText.getText().toString();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                mUserName = usernameEditText.getText().toString();
+                mPassword = passwordEditText.getText().toString();
+            }
+        });
+
+    }
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_login, menu);
---
+++ app/src/main/java/com/example/belov/wineapp/RegisterActivity.java
@@ -0,0 +1,9 @@
+package com.example.belov.wineapp;
+
+import android.app.Activity;
+
+/**
+ * Created by alexander on 11/17/2015.
+ */
+public class RegisterActivity extends Activity {
+}

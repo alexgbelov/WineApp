f6e4aacea5765dcb5e1e5ecd04242eba2ea9d249
Me <fezzler@gmail.com>
2015-12-03 09:47:12 -0500

    Added menu list and order activities

---
+++ app/src/main/java/com/example/belov/wineapp/Item.java
@@ -0,0 +1,13 @@
+package com.example.belov.wineapp;
+
+/**
+ * Created by Me on 12/2/2015.
+ */
+import android.graphics.Bitmap;
+
+public class Item {
+
+    String itemName;
+    String itemPrice;
+    Bitmap itemPicture;
+}

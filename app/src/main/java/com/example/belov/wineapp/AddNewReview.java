6132ba830f341a54c1fc94c06ce2df265356bd7b
petekarp <karpinator@gmail.com>
2015-12-04 16:01:06 -0500

    Added connection between itemInfo and order activity

--- app/src/main/java/com/example/belov/wineapp/AddNewReview.java
+++ app/src/main/java/com/example/belov/wineapp/AddNewReview.java
@@ -2,34 +2,56 @@ package com.example.belov.wineapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.parse.ParseHandler;
+
 public class AddNewReview extends AppCompatActivity{
+    Float starCount;
+    ParseHandler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_review);
         Button submitButton = (Button) findViewById(R.id.submitButton);
+        handler = ParseHandler.getParseHandler();
+        starCount = 0F;
         submitButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                ((RatingBar) findViewById(R.id.ratingBar)).setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+                    @Override
+                    public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        starCount = rating;
+                    }
+                });
 
-                //ADD submission code
+                String ratingText = ((EditText) findViewById(R.id.review)).getText().toString();
+
+                // TODO send starCount and ratingText to database
+                handler.uploadUserReview(getIntent().getStringExtra("itemId"), ratingText, Math.round(starCount) );
+
                 Toast toast = Toast.makeText(getApplicationContext(), "Review Submitted", Toast.LENGTH_SHORT);
                 toast.show();
-                Intent intent = new Intent(getApplicationContext(), ItemInformationActivity.class);
-                startActivity(intent);
+                finish();
+                //Intent intent = new Intent(getApplicationContext(), ItemInformationActivity.class);
+                //startActivity(intent);
 
             }
         });
-
         Button cancelButton = (Button) findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), ItemInformationActivity.class);
                 startActivity(intent);
             }
         });
+
+
     }
 }
--- app/src/main/java/com/example/belov/wineapp/ItemInformationActivity.java
+++ app/src/main/java/com/example/belov/wineapp/ItemInformationActivity.java
@@ -5,22 +5,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.example.parse.MenuItem;
+import com.example.parse.ParseHandler;
+
+import java.util.ArrayList;
 
 public class ItemInformationActivity extends AppCompatActivity {
-
+    ParseHandler handler;
+    ArrayList reviews;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_information);
 
+        TextView name =  (TextView) findViewById(R.id.name);
+        String itemName = getIntent().getStringExtra("itemName");
+        name.setText(itemName);
+        TextView description = (TextView) findViewById(R.id.description);
+        String itemDescription = getIntent().getStringExtra("itemDescription");
+        description.setText(itemDescription);
 
         Button addReviewButton = (Button) findViewById(R.id.addReviewButton);
         addReviewButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v){
                 Intent intent = new Intent(getApplicationContext(), AddNewReview.class);
+                intent.putExtra("itemId", getIntent().getStringExtra("itemId"));
                 startActivity(intent);
             }
         });
+
+        handler = ParseHandler.getParseHandler();
+        reviews = handler.getItemReviews(getIntent().getStringExtra("itemId"));
+
+        Button orderButton = (Button) findViewById(R.id.orderButton);
+        orderButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), OrderItemsActivity.class);
+                intent.putExtra("itemId", getIntent().getStringExtra("itemId"));
+                intent.putExtra("itemPrice", getIntent().getDoubleExtra("itemPrice", 0));
+                startActivity(intent);
+            }
+        });
+
     }
 
 
